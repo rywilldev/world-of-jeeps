ddfd3f6fb81905b9ecaef67323f3833fff6e87e5
Ryan D <rywilldav@gmail.com>
2024-04-19 18:21:33 -0400

    finish style and function of nav
    hamburger menu appears under 600px

---
+++ script.js
@@ -0,0 +1,8 @@
+function burgerToggle() {
+    var x = document.getElementById("topNav");
+    if (x.className === "nav") {
+      x.className += " responsive";
+    } else {
+      x.className = "nav";
+    }
+  }
